c3eb0d0c7730a4c5c68b9433b1015567ca540b6c
chidieb11 <chukwuchidieberejohn@gmail.com>
2022-05-12 20:35:48 +0100

    Initial commit

--- src/App.js
+++ src/App.js
@@ -3,94 +3,74 @@ import Login from "./pages/login/Login";
 import List from "./pages/list/List";
 import Single from "./pages/single/Single";
 import New from "./pages/new/New";
-import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
-import { productInputs, userInputs } from "./formSource";
+import {BrowserRouter, Routes, Route, Navigate} from "react-router-dom";
+import {productInputs, userInputs} from "./formSource";
 import "./style/dark.scss";
-import { useContext } from "react";
-import { DarkModeContext } from "./context/darkModeContext";
-import { AuthContext } from "./context/AuthContext";
+import {useContext} from "react";
+import {DarkModeContext} from "./context/darkModeContext";
+import {AuthContext} from "./context/AuthContext";
 
 function App() {
-  const { darkMode } = useContext(DarkModeContext);
+    const {darkMode} = useContext(DarkModeContext);
 
-  const { currentUser } = useContext(AuthContext);
-  console.log(currentUser);
+    const {currentUser} = useContext(AuthContext);
+    // const currentUser = false;
+    // console.log(currentUser);
 
-  const RequireAuth = ({ children }) => {
-    return currentUser ? children : <Navigate to="/login" />;
-  };
+    const RequireAuth = ({children}) => {
+        return currentUser ? children : <Navigate to="/login"/>;
+    };
 
-  return (
-    <div className={darkMode ? "app dark" : "app"}>
-      <BrowserRouter>
-        <Routes>
-          <Route path="/">
-            <Route
-              index
-              element={
-                <RequireAuth>
-                  <Home />
-                </RequireAuth>
-              }
-            />
-            <Route path="login" element={<Login />} />
-            <Route path="users">
-              <Route
-                index
-                element={
-                  <RequireAuth>
-                    <List />
-                  </RequireAuth>
-                }
-              />
-              <Route
-                path=":userId"
-                element={
-                  <RequireAuth>
-                    <Single />
-                  </RequireAuth>
-                }
-              />
-              <Route
-                path="new"
-                element={
-                  <RequireAuth>
-                    <New inputs={userInputs} title="Add New User" />
-                  </RequireAuth>
-                }
-              />
-            </Route>
-            <Route path="products">
-              <Route
-                index
-                element={
-                  <RequireAuth>
-                    <List />
-                  </RequireAuth>
-                }
-              />
-              <Route
-                path=":productId"
-                element={
-                  <RequireAuth>
-                    <Single />
-                  </RequireAuth>
-                }
-              />
-              <Route
-                path="new"
-                element={
-                  <RequireAuth>
-                    <New inputs={productInputs} title="Add New Product" />
-                  </RequireAuth>
-                }
-              />
-            </Route>
-          </Route>
-        </Routes>
-      </BrowserRouter>
-    </div>
-  );
+    return (
+        <div className={darkMode ? "app dark" : "app"}>
+            <BrowserRouter>
+                <Routes>
+                    <Route path="/">
+                        <Route index element={
+                            <RequireAuth>
+                                <Home/>
+                            </RequireAuth>
+                        }/>
+                        <Route path="login" element={<Login/>}/>
+                        <Route path="users">
+                            <Route index element={
+                                <RequireAuth>
+                                    <List/>
+                                </RequireAuth>
+                            }/>
+                            <Route path=":userId" element={
+                                <RequireAuth>
+                                    <Single/>
+                                </RequireAuth>
+                            }/>
+                            <Route path="new" element={
+                                <RequireAuth>
+                                    <New inputs={userInputs} title="Add New User"/>
+                                </RequireAuth>
+                            }/>
+                        </Route>
+                        <Route path="products">
+                            <Route index element={
+                                <RequireAuth>
+                                    <List/>
+                                </RequireAuth>
+                            }/>
+                            <Route path=":productId" element={
+                                <RequireAuth>
+                                    <Single/>
+                                </RequireAuth>
+                            }/>
+                            <Route path="new" element={
+                                <RequireAuth>
+                                    <New inputs={productInputs} title="Add New Product"/>
+                                </RequireAuth>
+                            }/>
+                        </Route>
+                    </Route>
+                </Routes>
+            </BrowserRouter>
+        </div>
+    );
 }
 
 export default App;
--- src/pages/new/New.jsx
+++ src/pages/new/New.jsx
@@ -2,57 +2,128 @@ import "./new.scss";
 import Sidebar from "../../components/sidebar/Sidebar";
 import Navbar from "../../components/navbar/Navbar";
 import DriveFolderUploadOutlinedIcon from "@mui/icons-material/DriveFolderUploadOutlined";
-import { useState } from "react";
+import {useEffect, useState} from "react";
+import {serverTimestamp, doc, setDoc} from "firebase/firestore";
+import {createUserWithEmailAndPassword} from "firebase/auth";
+import {auth, db, storage} from "../../firebase";
+import {ref, uploadBytesResumable, getDownloadURL} from "firebase/storage";
 
-const New = ({ inputs, title }) => {
-  const [file, setFile] = useState("");
+const New = ({inputs, title}) => {
+    const [file, setFile] = useState("");
+    const [data, setData] = useState("");
+    const [percentage, setPercentage] = useState(null);
 
-  return (
-    <div className="new">
-      <Sidebar />
-      <div className="newContainer">
-        <Navbar />
-        <div className="top">
-          <h1>{title}</h1>
-        </div>
-        <div className="bottom">
-          <div className="left">
-            <img
-              src={
-                file
-                  ? URL.createObjectURL(file)
-                  : "https://icon-library.com/images/no-image-icon/no-image-icon-0.jpg"
-              }
-              alt=""
-            />
-          </div>
-          <div className="right">
-            <form>
-              <div className="formInput">
-                <label htmlFor="file">
-                  Image: <DriveFolderUploadOutlinedIcon className="icon" />
-                </label>
-                <input
-                  type="file"
-                  id="file"
-                  onChange={(e) => setFile(e.target.files[0])}
-                  style={{ display: "none" }}
-                />
-              </div>
+    //Upload Image File
+    useEffect(() => {
+        const uploadFile = () => {
+            const name = new Date().getTime() + file.name;
+            console.log(name);
 
-              {inputs.map((input) => (
-                <div className="formInput" key={input.id}>
-                  <label>{input.label}</label>
-                  <input type={input.type} placeholder={input.placeholder} />
+            const storageRef = ref(storage, file.name);
+            const uploadTask = uploadBytesResumable(storageRef, file);
+
+            uploadTask.on(
+                "state_changed",
+                (snapshot) => {
+                    const progress =
+                        Math.round((snapshot.bytesTransferred / snapshot.totalBytes) * 100);
+                    console.log("Upload is " + progress + "% done");
+                    setPercentage(progress);
+                    switch (snapshot.state) {
+                        case "paused":
+                            console.log("Upload is paused");
+                            break;
+                        case "running":
+                            console.log("Upload is running");
+                            break;
+                        default:
+                            break;
+                    }
+                },
+                (error) => {
+                    console.log(error);
+                },
+                () => {
+                    getDownloadURL(uploadTask.snapshot.ref).then((downloadURL) => {
+                        setData((prev) => ({...prev, img: downloadURL}));
+                    });
+                }
+            );
+        };
+        file && uploadFile();
+    }, [file]);
+    console.log(data);
+
+    //Create User Input
+    const handleInput = (e) => {
+        const id = e.target.id;
+        const value = e.target.value;
+
+        setData({...data, [id]: value});
+    };
+    console.log(data);
+
+    //Add New User Input
+    const handleAdd = async (e) => {
+        e.preventDefault();
+        try {
+            const res = await createUserWithEmailAndPassword(auth, data.email, data.password);
+            await setDoc(doc(db, "users", res.user.uid), {
+                ...data,
+                timeStamp: serverTimestamp()
+            });
+        } catch (error) {
+            console.log(error);
+        }
+    };
+
+    return (
+        <div className="new">
+            <Sidebar/>
+            <div className="newContainer">
+                <Navbar/>
+                <div className="top">
+                    <h1>{title}</h1>
                 </div>
-              ))}
-              <button>Send</button>
-            </form>
-          </div>
+                <div className="bottom">
+                    <div className="left">
+                        <img
+                            src={
+                                file
+                                    ? URL.createObjectURL(file)
+                                    : "https://icon-library.com/images/no-image-icon/no-image-icon-0.jpg"
+                            }
+                            alt=""
+                        />
+                    </div>
+                    <div className="right">
+                        <form onSubmit={handleAdd}>
+                            <div className="formInput">
+                                <label htmlFor="file">
+                                    Image: <DriveFolderUploadOutlinedIcon className="icon"/>
+                                </label>
+                                <input
+                                    type="file"
+                                    id="file"
+                                    onChange={(e) => setFile(e.target.files[0])}
+                                    style={{display: "none"}}
+                                />
+                            </div>
+
+                            {inputs.map((input) => (
+                                <div className="formInput" key={input.id}>
+                                    <label>{input.label}</label>
+                                    <input type={input.type} placeholder={input.placeholder} id={input.id}
+                                           onChange={handleInput}/>
+                                </div>
+                            ))}
+                            <button disabled={percentage !== null && percentage < 100} type="submit">Send</button>
+                        </form>
+                    </div>
+                </div>
+            </div>
         </div>
-      </div>
-    </div>
-  );
+    );
 };
 
 export default New;
